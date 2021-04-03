071b89130f27b0aa13fb95334faf74186581632d
jjanmo <jjanmo07@hanmail.net>
2021-04-03 23:09:23 +0900

    Setup react project and add fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
 
-function App() {
+const App = () => {
+  const title = useRef('');
+
+  const getTitle = async () => {
+    console.log('fetch here');
+    const result = await fetch('/').json();
+    title.current = result.title;
+  };
+
+  useEffect(() => {
+    getTitle();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{title.current}</header>
     </div>
   );
-}
+};
 
 export default App;
