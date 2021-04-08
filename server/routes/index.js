8b2c5d9e3ca11325b41439da9d393edba176d10f
jjanmo <jjanmo07@hanmail.net>
2021-04-08 23:53:13 +0900

    Add join-router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res, next) => {
-  res.send({ title: 'Hello World, This is Simple React Node App 🥳' });
+router.get('/', (req, res) => {
+  res.send('Hello World 🔥');
+});
+
+router.post('/join', (req, res) => {
+  try {
+    // TODO need validatation : use express validator https://express-validator.github.io/docs/
+    const user = new User(req.body);
+
+    user.save((error, userInfo) => {
+      if (error) return res.json({ success: false, error });
+      return res.status(200).json({
+        success: true,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
