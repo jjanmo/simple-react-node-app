cf3b91e47e22ad8a25f28f85b3aadd4666ab03d2
jjanmo <jjanmo07@hanmail.net>
2021-04-09 00:47:45 +0900

    chore: delete trivial code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { home, postJoin } = require('../controllers');
-const User = require('../models/User');
 
 router.get('/', home);
 router.post('/join', postJoin);
