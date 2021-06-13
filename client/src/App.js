50754f37c76b357d7f30cf0357a915daf97502d3
jjanmo <jjanmo07@hanmail.net>
2021-06-13 22:33:49 +0900

    Update react project(client) structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,5 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
-const App = () => {
-  const title = useRef('');
-
-  const getTitle = async () => {
-    console.log('fetch here');
-    const result = await fetch('/');
-
-    // const data = JSON.parse();
-    console.log('✅', await result.json());
-    // title.current = result.title;
-  };
-
-  useEffect(() => {
-    getTitle();
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">{title.current}</header>
-    </div>
-  );
-};
+const App = () => <div className="App">Hello World</div>;
 
 export default App;
